22ed0825274703f4e11f0c00c2f8db4116b8e557
KangSeungChul <kang2411110208@gmail.com>
2025-04-24 11:30:49 +0900

    fix : 아이콘 이미지 변경 및 추가, 더미 데이터 구조 변경

--- js/script.js
+++ js/script.js
@@ -1,31 +1,155 @@
 // 데이터 배열
 const cardData = [
-    { title: '먹방 유튜버가 알려주는 음식 꿀조합', image_path: '0001.jpg', content: '먹방 유튜버가 알려주는 음식 꿀조합, 먹방 유튜버가 알려주는 음식 꿀조합, 먹방 유튜버가 알려주는 음식 꿀조합' },
-    { title: '연예인 꿀조합 서브웨이 레시피', image_path: '0002.jpg', content: '연예인 꿀조합 서브웨이 레시피, 연예인 꿀조합 서브웨이 레시피, 연예인 꿀조합 서브웨이 레시피' },
-    { title: '검증된 서브웨이 꿀팁', image_path: '0003.jpg', content: '검증된 서브웨이 꿀팁, 검증된 서브웨이 꿀팁, 검증된 서브웨이 꿀팁' },
-    { title: '매드포갈릭 매니아들만 알고 있었던 나만의 꿀조합', image_path: '0004.jpg', content: '매드포갈릭 매니아들만 알고 있었던 나만의 꿀조합, 매드포갈릭 매니아들만 알고 있었던 나만의 꿀조합, 매드포갈릭 매니아들만 알고 있었던 나만의 꿀조합' },
-    { title: '편의점 음식 꿀조합 레시피', image_path: '0005.png', content: '편의점 음식 꿀조합 레시피, 편의점 음식 꿀조합 레시피, 편의점 음식 꿀조합 레시피' },
-    { title: '추천 조합 한번에 주문하기', image_path: '0006.jpg', content: '추천 조합 한번에 주문하기, 추천 조합 한번에 주문하기, 추천 조합 한번에 주문하기' },
-    { title: '2인 배달음식 꿀조합', image_path: '0007.jpg', content: '2인 배달음식 꿀조합, 2인 배달음식 꿀조합, 2인 배달음식 꿀조합' },
-    { title: '카페별 음료 꿀조합', image_path: '0008.png', content: '카페별 음료 꿀조합, 카페별 음료 꿀조합, 카페별 음료 꿀조합' },
-    { title: '서브웨이 꿀조합 Top 7', image_path: '0009.webp', content: '서브웨이 꿀조합 Top 7, 서브웨이 꿀조합 Top 7, 서브웨이 꿀조합 Top 7' },
-    { title: '더미 게시글', image_path: '0010.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0011.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0012.png', content: '' },
-    { title: '더미 게시글', image_path: '0013.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0014.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0015.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0016.png', content: '' },
-    { title: '더미 게시글', image_path: '0017.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0018.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0019.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0020.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0021.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0022.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0023.png', content: '' },
-    { title: '더미 게시글', image_path: '0024.jpg', content: '' },
-    { title: '더미 게시글', image_path: '0025.jpg', content: '' },
-    // ... 추가 데이터
+    {
+        id: 0,
+        title: '먹방 유튜버가 알려주는 음식 꿀조합',
+        thumbnail_path: '0001.jpg',
+        content: '먹방 유튜버가 알려주는 음식 꿀조합, 먹방 유튜버가 알려주는 음식 꿀조합, 먹방 유튜버가 알려주는 음식 꿀조합'
+    },
+    {
+        id: 1,
+        title: '연예인 꿀조합 서브웨이 레시피',
+        thumbnail_path: '0002.jpg',
+        content: '연예인 꿀조합 서브웨이 레시피, 연예인 꿀조합 서브웨이 레시피, 연예인 꿀조합 서브웨이 레시피'
+    },
+    {
+        id: 2,
+        title: '검증된 서브웨이 꿀팁',
+        thumbnail_path: '0003.jpg',
+        content: '검증된 서브웨이 꿀팁, 검증된 서브웨이 꿀팁, 검증된 서브웨이 꿀팁'
+    },
+    {
+        id: 3,
+        title: '매드포갈릭 매니아들만 알고 있었던 나만의 꿀조합',
+        thumbnail_path: '0004.jpg',
+        content: '매드포갈릭 매니아들만 알고 있었던 나만의 꿀조합, 매드포갈릭 매니아들만 알고 있었던 나만의 꿀조합, 매드포갈릭 매니아들만 알고 있었던 나만의 꿀조합'
+    },
+    {
+        id: 4,
+        title: '편의점 음식 꿀조합 레시피',
+        thumbnail_path: '0005.png',
+        content: '편의점 음식 꿀조합 레시피, 편의점 음식 꿀조합 레시피, 편의점 음식 꿀조합 레시피'
+    },
+    {
+        id: 5,
+        title: '추천 조합 한번에 주문하기',
+        thumbnail_path: '0006.jpg',
+        content: '추천 조합 한번에 주문하기, 추천 조합 한번에 주문하기, 추천 조합 한번에 주문하기'
+    },
+    {
+        id: 6,
+        title: '2인 배달음식 꿀조합',
+        thumbnail_path: '0007.jpg',
+        content: '2인 배달음식 꿀조합, 2인 배달음식 꿀조합, 2인 배달음식 꿀조합'
+    },
+    {
+        id: 7,
+        title: '카페별 음료 꿀조합',
+        thumbnail_path: '0008.png',
+        content: '카페별 음료 꿀조합, 카페별 음료 꿀조합, 카페별 음료 꿀조합'
+    },
+    {
+        id: 8,
+        title: '서브웨이 꿀조합 Top 7',
+        thumbnail_path: '0009.webp',
+        content: '서브웨이 꿀조합 Top 7, 서브웨이 꿀조합 Top 7, 서브웨이 꿀조합 Top 7'
+    },
+    {
+        id: 9,
+        title: '더미 게시글',
+        thumbnail_path: '0010.jpg',
+        content: ''
+    },
+    {
+        id: 10,
+        title: '더미 게시글',
+        thumbnail_path: '0011.jpg',
+        content: ''
+    },
+    {
+        id: 11,
+        title: '더미 게시글',
+        thumbnail_path: '0012.png',
+        content: ''
+    },
+    {
+        id: 12,
+        title: '더미 게시글',
+        thumbnail_path: '0013.jpg',
+        content: ''
+    },
+    {
+        id: 13,
+        title: '더미 게시글',
+        thumbnail_path: '0014.jpg',
+        content: ''
+    },
+    {
+        id: 14,
+        title: '더미 게시글',
+        thumbnail_path: '0015.jpg',
+        content: ''
+    },
+    {
+        id: 15,
+        title: '더미 게시글',
+        thumbnail_path: '0016.png',
+        content: ''
+    },
+    {
+        id: 16,
+        title: '더미 게시글',
+        thumbnail_path: '0017.jpg',
+        content: ''
+    },
+    {
+        id: 17,
+        title: '더미 게시글',
+        thumbnail_path: '0018.jpg',
+        content: ''
+    },
+    {
+        id: 18,
+        title: '더미 게시글',
+        thumbnail_path: '0019.jpg',
+        content: ''
+    },
+    {
+        id: 19,
+        title: '더미 게시글',
+        thumbnail_path: '0020.jpg',
+        content: ''
+    },
+    {
+        id: 20,
+        title: '더미 게시글',
+        thumbnail_path: '0021.jpg',
+        content: ''
+    },
+    {
+        id: 21,
+        title: '더미 게시글',
+        thumbnail_path: '0022.jpg',
+        content: ''
+    },
+    {
+        id: 22,
+        title: '더미 게시글',
+        thumbnail_path: '0023.png',
+        content: ''
+    },
+    {
+        id: 23,
+        title: '더미 게시글',
+        thumbnail_path: '0024.jpg',
+        content: ''
+    },
+    {
+        id: 24,
+        title: '더미 게시글',
+        thumbnail_path: '0025.jpg',
+        content: ''
+    }
 ];
 
 function updateContentAndContainerHeight() {
@@ -102,6 +226,11 @@ function createCard(item, isPlaceholder = false) {
         `;
         card.appendChild(title);
 
+        const contentDiv = document.createElement('div');
+        contentDiv.className = 'card-content';
+        contentDiv.textContent = '임시 콘텐츠';
+        card.appendChild(contentDiv);
+
         // 마우스 이벤트
         card.addEventListener('mouseenter', () => {
             title.style.opacity = '1';
@@ -117,7 +246,7 @@ function createCard(item, isPlaceholder = false) {
 
     // 이미지
     const img = document.createElement('img');
-    img.src = "post_Tempdata/image/" + item.image_path;
+    img.src = "post_Tempdata/image/" + item.thumbnail_path;
     img.alt = item.title;
 
     // 타이틀
@@ -135,6 +264,12 @@ function createCard(item, isPlaceholder = false) {
         opacity: 0;
         transition: all 0.3s ease;
     `;
+
+    // Content 영역 추가 (PC에서 숨김)
+    const contentDiv = document.createElement('div');
+    contentDiv.className = 'card-content';
+    contentDiv.textContent = item.content || '';
+    card.appendChild(contentDiv);
 
     // 마우스 이벤트
     card.addEventListener('mouseenter', () => {
