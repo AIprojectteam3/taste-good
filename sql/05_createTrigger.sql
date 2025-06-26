2354dbe2ae999782b34a4cf25cd9e7cf2baf2f72
KangSeungChul <kang2411110208@gmail.com>
2025-06-26 16:44:51 +0900

    fix : 레벨업 자동으로 하는 프로시저 추가.

--- sql/05_createTrigger.sql
+++ sql/05_createTrigger.sql
@@ -28,3 +28,33 @@ DELIMITER ;
 
 -- 트리거 생성 완료 메시지
 SELECT 'User registration triggers created successfully!' AS message;
+
+-- =====================- 필요 경험치 도달했을 경우 레벨 자동 상승(프로시저) -=====================
+DELIMITER $$
+
+CREATE PROCEDURE add_experience(
+    IN p_user_id INT,
+    IN p_exp_amount INT
+)
+BEGIN
+    DECLARE current_level INT DEFAULT 1;
+    DECLARE new_experience INT DEFAULT 0;
+    
+    UPDATE user_levels 
+    SET experience = experience + p_exp_amount 
+    WHERE user_id = p_user_id;
+    
+    SELECT experience INTO new_experience 
+    FROM user_levels 
+    WHERE user_id = p_user_id;
+    
+    SELECT MAX(level) INTO current_level
+    FROM level_requirements 
+    WHERE required_exp <= new_experience;
+    
+    UPDATE user_levels 
+    SET level = current_level 
+    WHERE user_id = p_user_id;
+END$$
+
+DELIMITER ;
